537bf2ddec2e8905b9c3ab1aa0388a1a6ef3fb62
ziddihacker2 <74632083+ziddihacker2@users.noreply.github.com>
2021-03-24 08:23:15 +0530

    Add files via upload

---
+++ shutter.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+
+def shutdown():
+    print("\n \033[01;36m Your Computer is shutdown\n")
+
+def restart():
+    print("\n \033[01;36m Your Computer is Restart\n")
+
+def lock():
+    print('\n \033[01;36m Your computer is lock\n')
+
+def logout():
+    print('\n \033[01;36m Your computer is logout\n')
+
+print("\n \033[01;32m Choose Options Restart, Shutdown, Lock and Logout\n")
+
+computer = ''
+
+while computer != 'q':
+    print("\t \033[01;35m Ziddi Hacker 2!!")
+    print('\n \033[01;36m[1] Enter 1 to your computer is shutdown')
+    print('\033[01;36m [2] Enter 2 to your computer is restart')
+    print("\033[01;36m [3] Enter 3 to Lock Your computer")
+    print('\033[01;36m [4] Enter 4 to Logout Your computer')
+    print('\033[01;36m [q] Enter q to exit')
+
+    computer = input('\033[01;36m Choose Options and Hit enter: ')
+    if computer == '1':
+        shutdown()
+        os.system("shutdown /s /t 1")
+    elif computer == '2':
+        restart()
+        os.system("shutdown /r /t 1")
+    elif computer == '3':
+        lock()
+        cmd='rundll32.exe user32.dll, LockWorkStation'
+        subprocess.call(cmd)
+    elif computer == '4':
+        logout()
+        os.system('shutdown -l')
+    elif computer == 'q':
+        print("\n \033[01;36m Thanks")
+    else:
+        print("\n \033[01;31m Thanks! But Sorry Please try again another command\n")
+print("\033[01;37m Created By Ziddi Hacker 2!")
